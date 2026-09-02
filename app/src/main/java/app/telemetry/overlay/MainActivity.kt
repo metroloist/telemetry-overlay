2da9cd9e380ff7bffafaa2598cba4dac3c8d3f1f
metroloist <93645216+metroloist@users.noreply.github.com>
2026-09-02 17:29:03 +0700

    Fix Compose activity import

--- app/src/main/java/app/telemetry/overlay/MainActivity.kt
+++ app/src/main/java/app/telemetry/overlay/MainActivity.kt
@@ -4,6 +4,7 @@ import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
